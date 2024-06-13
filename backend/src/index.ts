ecef6d9c13f835fa806dc736c40b689a9286a30a
vasanth064 <vasanthvdev@gmail.com>
2024-06-13 18:42:18 +0530

    modifed index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ import logger from './config/logger';
 let server: Server;
 prisma.$connect().then(() => {
   logger.info('Connected to SQL Database');
-  server = app.listen(config.port, () => {
+  server = app.listen(config.port, '0.0.0.0', () => {
     logger.info(`Listening to port ${config.port}`);
   });
 });
@@ -36,8 +36,4 @@ process.on('SIGTERM', () => {
   if (server) {
     server.close();
   }
-});
-
-app.listen(config.port,'0.0.0.0', () => {
-  logger.info(`Listening to port ${config.port}`);
 });
