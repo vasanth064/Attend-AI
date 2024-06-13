14f1591aecd4ba21e93d5688fff7a2ee56661b61
vasanth064 <vasanthvdev@gmail.com>
2024-06-13 18:59:06 +0530

    health check and admin create route

--- backend/src/routes/v1/index.ts
+++ backend/src/routes/v1/index.ts
@@ -6,8 +6,33 @@ import adminRoute from './admin.route';
 import config from '../../config/config';
 import clientRoute from './client.route';
 import machineRoute from './machine.route';
+import prisma from '../../client';
 
 const router = express.Router();
+
+router.route('/').get(async (req, res) => {
+  const admin = await prisma.user.findFirst({
+    where: {
+      email: 'admin@attendai.com'
+    }
+  });
+  if (!admin) {
+    await prisma.user.create({
+      data: {
+        email: 'admin@attendai.com',
+        password: 'admin@attendai.com',
+        name: 'admin',
+        userType: 'ADMIN'
+      }
+    });
+    return res.status(200).json({
+      message: 'Working :))'
+    });
+  }
+  res.status(200).json({
+    message: 'Working :)'
+  });
+});
 
 const defaultRoutes = [
   {
